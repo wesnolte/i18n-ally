6ed5ad49427f09247752ad94dae07658fffb4f8d
wes <wes@wes.wes>
2023-11-09 15:16:08 +0000

    i18n-ally will use namespace set by next-translates useTranslation('namespace') hook

--- src/frameworks/next-translate.ts
+++ src/frameworks/next-translate.ts
@@ -1,4 +1,5 @@
-import { Framework } from './base'
+import { TextDocument } from 'vscode'
+import { Framework, ScopeRange } from './base'
 import { LanguageId } from '~/utils'
 
 class NextTranslateFramework extends Framework {
@@ -36,6 +37,41 @@ class NextTranslateFramework extends Framework {
 
   rewriteKeys(key: string) {
     return key.replace(/:/g, '.')
+  }
+
+  // useTranslation
+  getScopeRange(document: TextDocument): ScopeRange[] | undefined {
+    if (![
+      'javascript',
+      'typescript',
+      'javascriptreact',
+      'typescriptreact',
+    ].includes(document.languageId))
+      return
+
+    const ranges: ScopeRange[] = []
+    const text = document.getText()
+    const reg = /useTranslation\((?:['"`](.*)['"`]|)\)/g
+
+    for (const match of text.matchAll(reg)) {
+      if (match?.index == null)
+        continue
+
+      // end previous scope
+      if (ranges.length)
+        ranges[ranges.length - 1].end = match.index
+
+      // start new scope if namespace provides
+      if (match[1]) {
+        ranges.push({
+          start: match.index,
+          end: text.length,
+          namespace: match[1] as string,
+        })
+      }
+    }
+
+    return ranges
   }
 
   pathMatcher() {
